795dc9f88bb163edcc2deba9ce05d2a9486891b8
user419 <user419@cyber.sibsutis.ru>
2016-09-21 09:31:01 +0600

    TOP4ik refact. code

--- 1.cpp
+++ 1.cpp
@@ -15,46 +15,69 @@ int main() {
     int *sec = new int[n*m];
     int **array = new int* [n]; 
     
-    for(int i = 0; i < n; i++){
+    int l = 0;
+    int i = 0;
+    int j = 0;
+    
+    for(i = 0; i < n; i++){
         array[i] = new int [m];
     }
-    
-     
-    
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
+    cout << "\t\tKRASIVO" << endl;
+    for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
             array[i][j] = 10 + rand() % 20;        
             cout << setw(3) << array[i][j] << "  ";            
         }
         cout << endl << endl;
     }
-    cout << endl;
+    cout << endl << endl;
+    
     // LEFT
-    /*for(int i = 0; i < n; ++i){
-        for(int j = 0 ; j < n-i; ++j){
-            sec[r++] = array[j][i+(c++)];
-            sec[r++] = array[i+(c++)][j];
-            cout << setw(2) << sec[r-1] << " ";
-        }
-    c = 0; 
-    }    
-    cout << endl;*/
+    cout << "\t\tLEFT D" << endl;
+    l = 0;
+	for (int k = 0; k < n; k++){
+		j = k;
+		for (i = 0; i < n && j >= 0; i++)
+			sec[l++] = array[i][j--];
+	}
+	
+	for (int k = 1; k < n; k++){
+		i = k;
+		for (j = n - 1; i < n; j--)
+			sec[l++] = array[i++][j];
+	}
+	
+	printf ("\n");
+	
+	for (i = 0; i < l; i++)
+		printf ("%3d", sec[i]);
     
     // RIGHT
-    /*for(int i = n-1; i >= 0; --i){
-        for(int j = 0; j <= i; ++j){
-            sec[r++] = array[j][i-(c++)];
-            cout << setw(2) << sec[r-1] << " ";
-        }
-    c = 0; 
-    }    
-    cout << endl;*/
-    
-    
+    cout << endl << endl;
+    cout << "\t\tRIGHT D" << endl;
+    l = 0;
+	for (int k = n - 1; k >= 0; k--){
+		j = k;
+		for (i = 0; i < n && j < n; i++)
+			sec[l++] = array[i][j++];
+	}
+	
+	for (int k = 1; k < n; k++){
+		i = k;
+		for (j = 0; i < n; j++)
+			sec[l++] = array[i++][j];
+	}
+	printf ("\n");
+	for (i = 0; i < l; i++)
+		printf ("%3d", sec[i]);
+    cout << endl;
+    cout << endl << endl;
     // SPIROL
-    int i = n/2;
-    int j = n/2;
+    
+    i = n/2;
+    j = n/2;
     int p = 0;
+    cout << "\t\tSPIROL CENTER" << endl;
     do{
         p++;
         for (int k = 0; k < p; k++)
@@ -70,6 +93,28 @@ int main() {
             cout << array[i][j--] << ' ';
     }while (p);
     
+    cout << endl << endl;
+    cout << endl << endl;
+    cout << "\t\tSPIROL TOP LEFT" << endl;
+    l = 0;
+    j;
+    i = 0;
+    for (int k = 0; k < (n / 2 + n % 2); k++){
+		for (j = k; j < n - k; j++)
+			sec[l++] = array[k][j];
+		if (j == (n / 2 + n % 2)) break;
+		j -= 1;
+		for (i = 1 + k; i < n - k; i++)
+			sec[l++] = array[i][j];
+		i -= 1;
+		for (j = n - k - 2; j >= 0 + k; j--)
+			sec[l++] = array[i][j];
+		j += 1;
+		for (i = n - k - 2; i > 0 + k; i--)
+			sec[l++] = array[i][j];
+	}   
+    for (int i = 0; i < l; i++)
+		printf ("%3d", sec[i]); 
     cout << endl << endl << endl;      
     
     for(int i = 0; i < n; i++){
